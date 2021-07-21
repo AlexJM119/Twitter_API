8fd18461fb8740783c39f00fa7253414ebe6175e
AlexJM119 <86691859+AlexJM119@users.noreply.github.com>
2021-07-21 19:58:37 -0400

    Update twitter_api.py

--- twitter_api.py
+++ twitter_api.py
@@ -1,11 +1,11 @@
-import requests 
+import requests
 
 CLIENT_ID = 'aRaHc4eMk3inYP4phEv2Razi9'
 CLIENT_SECRET = 'Vw0V8fBoTDSWmkKPpzCjgN0RymA4a50kwR3Jnj9nxRPE01gO2T'
 
 URL = 'https://api.twitter.com/oauth2/token'
 auth_response = requests.post(URL, {
-    'grant_type' : 'client_credentials', 
+    'grant_type': 'client_credentials', 
     'client_id': CLIENT_ID, 
     'client_secret': CLIENT_SECRET
 })
@@ -27,4 +27,3 @@ tweet_id = '1417431102390603776'
 r = requests.get(BASE_URL + tweet_id, headers=headers)
 r = r.json()
 print(f"\nTweet Specs\nid : {r['data']['id']}\ntext: {r['data']['text']}")
-

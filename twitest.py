5e08e15a4cc0282f2de21a6b1a495db11dd2c7c3
Alexander Morrison <ajmorrison119@gmail.com>
2021-07-23 21:58:36 +0000

    adding unit-tests

---
+++ twitest.py
@@ -0,0 +1,25 @@
+import unittest
+from twitter_api import get_text, get_response, to_json, make_request
+
+CLIENT_ID = 'aRaHc4eMk3inYP4phEv2Razi9'
+CLIENT_SECRET = 'Vw0V8fBoTDSWmkKPpzCjgN0RymA4a50kwR3Jnj9nxRPE01gO2T'
+
+URL = 'https://api.twitter.com/oauth2/token'
+
+BASE_URL = 'https://api.twitter.com/2/tweets/'
+
+tweet_id = '1418562793586966528'
+
+database_name = 'TweBase'
+
+class Twitest(unittest.TestCase):
+
+    def test_get_text(self):
+        response = get_response(url=URL, c_id=CLIENT_ID, c_secret=CLIENT_SECRET)
+        resp = to_json(response)
+        req = make_request(BASE_URL, tweet_id, resp)
+        r = to_json(req)
+        self.assertNotEqual(get_text(r), "")
+
+if __name__ == '__main__':
+    unittest.main()
--- twitter_api.py
+++ twitter_api.py
@@ -7,32 +7,44 @@ CLIENT_ID = 'aRaHc4eMk3inYP4phEv2Razi9'
 CLIENT_SECRET = 'Vw0V8fBoTDSWmkKPpzCjgN0RymA4a50kwR3Jnj9nxRPE01gO2T'
 
 URL = 'https://api.twitter.com/oauth2/token'
-auth_response = requests.post(URL, {
-    'grant_type': 'client_credentials',
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET
-})
-print(auth_response.status_code)
-
-auth_response_data = auth_response.json()
-print(auth_response_data)
-
-access_token = auth_response_data['access_token']
-
-headers = {
-    'Authorization': 'Bearer {token}'.format(token=access_token)
-}
 
 BASE_URL = 'https://api.twitter.com/2/tweets/'
 
-tweet_id = '1417431102390603776'
+tweet_id = '1418562793586966528'
 
-r = requests.get(BASE_URL + tweet_id, headers=headers)
-r = r.json()
-# print(f"\nTweet Specs\nid : {r['data']['id']}\ntext: {r['data']['text']}")
+database_name = 'TweBase'
 
-twidata = pd.DataFrame.from_dict(r)
-print(twidata)
+def get_response(url, c_id, c_secret):
+    return requests.post(url, {'grant_type': 'client_credentials', 'client_id': c_id, 'client_secret': c_secret})
 
-engine = create_engine('mysql://root:codio@localhost/TwiBase')
-twidata.to_sql('tweet_data', con=engine, if_exists='replace', index=False)
+def get_status_code(response):
+    return response.status_code
+
+def to_json(response):
+    return response.json()
+  
+def make_request(base, tweet, json_input):
+    head = {'Authorization': 'Bearer {token}'.format(token=json_input['access_token'])}
+    return requests.get(base + tweet, headers=head)
+
+def create_dataframe(request_input):
+    df = pd.DataFrame.from_dict(request_input)
+    return df
+
+def start_engine(db, db_name):
+    engine = create_engine(f'mysql://root:codio@localhost/{db_name}')
+    db.to_sql('tweet_data', con=engine, if_exists='replace', index=False)
+
+def get_text(req_json):
+    return req_json['data']['text']
+
+response = get_response(url=URL, c_id=CLIENT_ID, c_secret=CLIENT_SECRET)
+print(get_status_code(response))
+
+resp = to_json(response)
+
+req = make_request(BASE_URL, tweet_id, resp)
+r = to_json(req)
+
+twedata = create_dataframe(r)
+start_engine(twedata, database_name)
